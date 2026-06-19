bea20d85bac82dbc4c03b3afb3959a3d4ee84507
Ina Sharma <142999200+Coderisa@users.noreply.github.com>
2026-06-19 20:15:26 +0530

    Update recommender_api.py

--- ml-service/recommender_api.py
+++ ml-service/recommender_api.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # CORS – allow React frontend (adjust ports as needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173",startup-vidyapith-ten.vercel.app],
+    allow_origins=["http://localhost:3000", "http://localhost:5173","startup-vidyapith-ten.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
